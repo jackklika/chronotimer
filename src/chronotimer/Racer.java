e99e79ae51d738519b41d9fd31282131040008a3
kmontour <kmontour@CEAS-E270-PC04.ad.uwm.edu>
2017-03-02 11:35:19 -0600

    racer has a runtime

--- src/chronotimer/Racer.java
+++ src/chronotimer/Racer.java
@@ -3,8 +3,13 @@ package chronotimer;
 public class Racer {
 	public Time t = new Time();
 	public int bib;
-	
-public Racer(int bib){
-	this.bib = bib;
-}
+	public long runTime;
+
+	public Racer(int bib) {
+		this.bib = bib;
+	}
+
+	public void getRunTime() {
+		runTime = t.runTime();
+	}
 }
