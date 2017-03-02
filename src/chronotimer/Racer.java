602c7482e06c3e2cba3c96bda9bff60cfeafe9e3
kmontour <kmontour@CEAS-E270-PC04.ad.uwm.edu>
2017-03-02 12:10:10 -0600

    swap is in racer class. it does not work

--- src/chronotimer/Racer.java
+++ src/chronotimer/Racer.java
@@ -1,5 +1,10 @@
 package chronotimer;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+
 public class Racer {
 	public Time t = new Time();
 	public int bib;
@@ -11,5 +16,12 @@ public class Racer {
 
 	public void getRunTime() {
 		runTime = t.runTime();
+	}
+	
+public void swap(){
+		
+		ArrayList list = new ArrayList(racers);
+		Collections.swap(list, 0, 1);
+	
 	}
 }
